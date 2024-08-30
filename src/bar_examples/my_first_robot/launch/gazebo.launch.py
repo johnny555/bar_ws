ba2328129d00db3e5d8e22e3aaa8d7cd9da21fab
John Vial <badwolf.johnnyv@gmail.com>
2024-08-30 02:40:24 +0000

    Adding files and updates

--- src/bar_examples/my_first_robot/launch/gazebo.launch.py
+++ src/bar_examples/my_first_robot/launch/gazebo.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
         else:
             os.environ["SDF_PATH"] = gz_sim_resource_path
 
-    cafe_world_uri = join(get_package_share_directory("gamecity"), "worlds", "gamecity.sdf")
+    cafe_world_uri = join(get_package_share_directory("my_first_robot"), "worlds", "warehouse.sdf")
    
 
     # Gazebo Sim.
