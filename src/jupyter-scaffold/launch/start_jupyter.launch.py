235af69257e86f39504c02b32b38351d1d8e760c
John Vial <badwolf.johnnyv@gmail.com>
2024-09-27 12:52:11 +0800

    Finalising basic examples

--- src/jupyter-scaffold/launch/start_jupyter.launch.py
+++ src/jupyter-scaffold/launch/start_jupyter.launch.py
@@ -1,19 +1,9 @@
-import os
 from os.path import join
-
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import ExecuteProcess
 from launch.actions import IncludeLaunchDescription
-from launch.conditions import IfCondition
-from launch.conditions import UnlessCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch.substitutions import PathJoinSubstitution
 
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
